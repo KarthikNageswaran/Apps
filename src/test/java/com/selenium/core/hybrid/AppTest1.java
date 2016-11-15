5098919efd23d042a2191e1f3717a7e097e24740
KarthikNageswaran <karthick.information@gmail.com>
2016-11-15 15:47:58 +0530

    added new code

---
+++ src/test/java/com/selenium/core/hybrid/AppTest1.java
@@ -0,0 +1,12 @@
+package com.selenium.core.hybrid;
+
+public class AppTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("GIT Checking in");
+
+	}
+
+}
