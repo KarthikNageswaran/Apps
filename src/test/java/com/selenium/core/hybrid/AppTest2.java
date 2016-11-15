1dee8b0f94cb66caf5bced36e56348c815b8aefe
KarthikNageswaran <karthick.information@gmail.com>
2016-11-15 16:41:14 +0530

    updating little change

--- src/test/java/com/selenium/core/hybrid/AppTest2.java
+++ src/test/java/com/selenium/core/hybrid/AppTest2.java
@@ -4,7 +4,7 @@ public class AppTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.Out.Println("Testing 1");
+		System.out.print("Testing 1");
 
 	}
 
